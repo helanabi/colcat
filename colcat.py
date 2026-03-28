487358f3507a584f3af671ef9642cdaff711084c
Hassan El anabi <abulkeram@proton.me>
2026-03-28 09:14:54 +0000

    Support column mapping file

--- colcat.py
+++ colcat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import json
 import sys
 import pandas as pd
 
@@ -13,13 +14,29 @@ def parse_args():
         epilog=f"{VERSION} copyright (c) 2026 Hassan El anabi"
     )
     parser.add_argument("file", nargs='+', help="input data files")
+    parser.add_argument("-m", "--mapping", metavar="JSON",
+                        help="JSON file mapping column names")
     parser.add_argument("-o", "--output", default="output.xlsx",
                         help="output file name")
     parser.add_argument("-v", "--version", action="version", version=VERSION)
     return parser.error, parser.parse_args()
 
-def normalize(name):
-    return name.strip().capitalize()
+def load_mapping(mapping_file):
+    with open(mapping_file) as f:
+        mapping = json.load(f)
+    if not isinstance(mapping, dict):
+        error("Error: mapping file must consist of a mapping object", 4)
+    if not all(map(lambda obj: isinstance(obj, list), mapping.values())):
+        error("Error: mapping object values must be lists", 4)
+
+    def normalize(name):
+        name = name.strip()
+        name_lower = name.lower()
+        for normalized, alt in mapping.items():
+            if name_lower in alt or name_lower == normalized.lower():
+                return normalized
+        return name
+    return normalize
 
 def load_file(filename):
     if filename.endswith(".csv"):
@@ -35,6 +52,11 @@ def main():
         if not any(filename.endswith(ext) for ext in (".csv", ".xlsx")):
             usage_error(f"unsupported file: {filename}")
 
+    if args.mapping:
+        normalize = load_mapping(args.mapping)
+    else:
+        normalize = lambda name: name.strip().capitalize()
+
     cols = []
     frames = tuple(map(load_file, args.file))
     for header in map(lambda df: df.columns.values, frames):
@@ -48,10 +70,17 @@ def main():
     if not output.endswith(".xlsx"):
         output += ".xlsx"
     pd.concat(frames, ignore_index=True).to_excel(output, index=False)
-            
+
+def error(msg, code):
+    print(str(msg), file=sys.stderr)
+    sys.exit(code)
+
 if __name__ == "__main__":
     try:
         main()
+    except (FileNotFoundError, PermissionError) as e:
+        error(e, 3)
+    except (json.JSONDecodeError, UnicodeDecodeError) as e:
+        error(e, 4)
     except Exception as e:
-        print(e, file=sys.stderr)
-        sys.exit(9)
+        error(e, 9)
